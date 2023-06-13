e386fa994237be77c2eec9fa7d14eaa899fb5248
William Jin <williamjinq@outlook.com>
2023-06-13 15:45:00 +0800

    fixed widget height issues

--- components/waitlist.js
+++ components/waitlist.js
@@ -3,9 +3,10 @@ import Helmet from "react-helmet";
 export default function NewWidget() {
     return (
         <>
-            <div className="justify-center h-60"
+            <div className="justify-center"
                 id="getWaitlistContainer"
                 data-waitlist_id="8125"
+                style={{ minHeight: 233 }}
             ></div>
             <Helmet>
                 <link
